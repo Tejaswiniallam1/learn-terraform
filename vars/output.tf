f36e93222cd0fcba31b8ce65690ae99dbecd65bc
tejudevops <saitejaswini8121@gmail.com>
2024-12-12 23:05:42 +0530

    vars ,maps, list

--- vars/output.tf
+++ vars/output.tf
@@ -1,3 +1,12 @@
 output "v1" {
   value = var.v1
 }
+output "First_value_in_list" {
+  value = var.v2[0]
+}
+output "second_value_in_list" {
+  value = var.v2[1]
+}
+output "value_in_map" {
+  value = var.v3["abc"]
+}
--- vars/variables.tf
+++ vars/variables.tf
@@ -1,3 +1,12 @@
 variable "v1" {
   default = "Hello World"
 }
+variable "v2" {
+  default = ["hello world", 100, true]
+}
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "two hundred"
+  }
+}
