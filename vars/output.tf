f5bb8901b685ef9832f6fb676ef4157dfe92f2e7
tejudevops <saitejaswini8121@gmail.com>
2024-12-12 23:23:29 +0530

    vars ,maps, list

--- vars/output.tf
+++ vars/output.tf
@@ -10,3 +10,8 @@ output "second_value_in_list" {
 output "value_in_map" {
   value = var.v3["abc"]
 }
+output "Value_in_map1" {
+  value = lookup(var.v3, "abc1", "none")
+}
+output "second_value_in_list1" {
+  value = element(var.v2, 4 )
