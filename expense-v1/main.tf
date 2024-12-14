22ee248bea63777cd82730c1a0d221865eaf1ce0
tejudevops <saitejaswini8121@gmail.com>
2024-12-14 19:06:15 +0530

    expense-v1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami                    = ami-0b4f379183e5706b9
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-0434fb24f059185a8]
+  vpc_security_group_ids = ["sg-0434fb24f059185a8"]
 
   tags = {
     Name = "frontend.dev"
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami                    = ami-0b4f379183e5706b9
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-0434fb24f059185a8]
+  vpc_security_group_ids = ["sg-0434fb24f059185a8"]
 
   tags = {
     Name = "backend.dev"
@@ -19,7 +19,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami                    = ami-0b4f379183e5706b9
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-0434fb24f059185a8]
+  vpc_security_group_ids = ["sg-0434fb24f059185a8"]
 
   tags = {
     Name = "mysql.dev"
