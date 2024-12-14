1959651f49315d0156a00f3a5d31ff010e8e8347
tejudevops <saitejaswini8121@gmail.com>
2024-12-14 19:38:05 +0530

    expense-v1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
     Name = "frontend.dev"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = Z042604511V76I8BWBZWX
+  name    = "frontend.dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "backend" {
   ami                    = "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
@@ -16,6 +24,14 @@ resource "aws_instance" "backend" {
     Name = "backend.dev"
   }
 }
+resource "aws_route53_record" "backend" {
+  zone_id = Z042604511V76I8BWBZWX
+  name    = "backend.dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.backend.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami                    = "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
@@ -24,4 +40,11 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql.dev"
   }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = Z042604511V76I8BWBZWX
+  name    = "mysql.dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
 }
