df6057b456859e812b8b8fa1b44743ab1a9e0d96
tejudevops <saitejaswini8121@gmail.com>
2024-12-14 22:41:13 +0530

    terraform main code

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -5,14 +5,15 @@ resource "aws_instance" "instances" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = element(var.components,count.index )
+    Name = element(var.components, count.index )
   }
 }
 resource "aws_route53_record" "records" {
+  count   = length(var.components)
   zone_id = var.zone_id
   name    = "${element(var.components,count.index)}.dev"
   type    = "A"
-  ttl     = 300
+  ttl     = 30
   records = [element(aws_instance.instances.*.private_ip,count.index )]
 }
 
