d3cc4adb3ac2a6f8c6e5c5abf29abf1e15c2446d
tejudevops <saitejaswini8121@gmail.com>
2024-12-15 14:24:14 +0530

    terraform main code

---
+++ expense-v4/vars.tf
@@ -0,0 +1,25 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-0434fb24f059185a8"]
+}
+variable "zone_id" {
+  default = "Z042604511V76I8BWBZWX"
+}
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name          = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+  }
+}
